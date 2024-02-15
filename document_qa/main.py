9614d280acaa3548f1576615001f2f24c3e76b0d
Arun Gautham Soundarrajan <arungautham60@gmail.com>
2024-02-15 21:15:32 +0000

    Basic working version

--- document_qa/main.py
+++ document_qa/main.py
@@ -1,23 +1,36 @@
-# from vector_search.vector_db import create_db
-from pprint import pprint
+from contextlib import asynccontextmanager
 
-from text_extracter.pdf_miners import extract_text
-from vector_search.text_splitter import TextSplitter
-from vector_search.vector_db import create_db
+from database.database import engine
+from database.models import Base
+from fastapi import FastAPI, Response, Request
+from fastapi.middleware.cors import CORSMiddleware
+from routers.v1 import files, search
+from vector_search.text_embedders import TextEmbedding
 
-
-def main():
-    pdf_path = "document_qa/documents/general-accident-financial-statements.pdf"
-
-    text = extract_text(pdf_path=pdf_path, pages=(0, 5))
-
-    ts = TextSplitter(text=text, preprocessing=True)
-    docs = ts.st_split_by_token(tokens_per_chunk=384, chunk_overlap=30)
-    db = create_db(documents=docs)
-    query = "What did the president say about Ketanji Brown Jackson"
-    docs = db.similarity_search(query)
-    print(docs[0].page_content)
+Base.metadata.create_all(bind=engine)
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield {"text_embedder": TextEmbedding()}
+
+
+app = FastAPI(lifespan=lifespan)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(files.router)
+app.include_router(search.router)
+
+
+@app.get("/")
+async def health(request: Request):
+    print(request.state.model)
+    return Response("Server is running.")
--- document_qa/text_extracter/pdf_miners.py
+++ document_qa/text_extracter/pdf_miners.py
@@ -1,15 +1,41 @@
+from io import BytesIO
+from pathlib import Path
+from typing import Callable, Dict, List
+
 import fitz
+from vector_search.text_embedders import TextEmbedding
 
 
-def extract_text(pdf_path: str, pages: tuple[int, int] | None = None) -> str:
-    extracted_text = ""
-    with fitz.open(pdf_path) as doc:
+def extract_text(
+    file_name: str,
+    file_content: bytes,
+    doc_id: str,
+    text_splitter: Callable,
+    params: Dict,
+    embedder: TextEmbedding,
+    pages: tuple[int, int] | None = None,
+) -> List[Dict]:
+
+    extracted_text = []
+    with fitz.open(filetype="pdf", stream=BytesIO(file_content)) as doc:
         if pages:
             selected_pages = doc.pages(*pages)
         else:
             selected_pages = doc
 
-        for page in selected_pages:
-            extracted_text += page.get_text("text")
+        for idx, page in enumerate(selected_pages):
+            text = page.get_text("text")
+
+            splitted_text = text_splitter(text=text, **params)
+            for doc in splitted_text:
+                extracted_text.append(
+                    {
+                        "doc_id": doc_id,
+                        "text": doc,
+                        "page_number": pages[0] + idx if pages else idx + 1,
+                        "document_name": file_name,
+                        "embeddings": embedder.embed_documents(doc),
+                    }
+                )
 
     return extracted_text
